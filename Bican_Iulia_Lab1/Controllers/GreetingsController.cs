cdaab3b831f13e480720736101231602358fd73b
Iulia Bican <67172566+iuliabican@users.noreply.github.com>
2022-10-11 19:39:15 +0300

    Lab 1 final (3 metode testate)

---
+++ Bican_Iulia_Lab1/Controllers/GreetingsController.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Bican_Iulia_Lab1.Controllers
+{
+    public class GreetingsController : Controller
+    {
+
+        public string Index()
+        {
+            string x = "Welcome to this ASP.NET Homepage";
+            return x;
+        }
+
+        public string Hello()
+        {
+            string x = "Hello User!";
+            return x;
+        }
+
+        public string HelloWithInput(string name = "Gica", int age = 25)
+        {
+            
+            return "Hello " + name + ", you are " + age + " years old.";
+        }
+    }
+}
