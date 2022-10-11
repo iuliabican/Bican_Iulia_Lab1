f5b8d4b2247ef30cfc29beccb44f9c51bafb47bd
Iulia Bican <67172566+iuliabican@users.noreply.github.com>
2022-10-11 19:10:18 +0300

    Add project files.

---
+++ Bican_Iulia_Lab1/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace Bican_Iulia_Lab1.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
